819752be488499222ba870e44924e98223839283
maiizii <59905570+maiizii@users.noreply.github.com>
2025-10-09 23:15:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ PROJECT_VERSION="v1.10.9.2"
 APT_UPDATED=false
 
 print_banner() {
-  printf '
-  SubLink 短链子域管理平台%s 一键安装脚本\n\n' "$PROJECT_VERSION"
+  printf '\n\nSubLink 短链子域管理平台%s 一键安装脚本\n\n' "$PROJECT_VERSION"
   cat <<'BANNER'
 ███████╗██╗   ██╗██████╗ ██╗     ██╗███╗   ██╗██╗  ██╗
 ██╔════╝██║   ██║██╔══██╗██║     ██║████╗  ██║██║ ██╔╝
@@ -18,7 +17,7 @@ print_banner() {
 ╚════██║██║   ██║██╔══██╗██║     ██║██║╚██╗██║██╔═██╗
 ███████║╚██████╔╝██████╔╝███████╗██║██║ ╚████║██║  ██╗
 ╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
-                                   Powered by MaiiZii
+                                   Powered by MaiiZii\n\n
                                    
 BANNER
 }
