8f2d0e609c56f1302d4e7638204b9030d7dd8581
maiizii <59905570+maiizii@users.noreply.github.com>
2025-10-10 12:08:55 +0800

    Ensure branch fetch in update

--- install.sh
+++ install.sh
@@ -159,6 +159,10 @@ update_repo() {
   verify_branch
   log_step "拉取最新代码"
   git -C "$INSTALL_DIR" fetch --all --prune
+  if ! git -C "$INSTALL_DIR" fetch --depth 1 origin "$BRANCH"; then
+    log_error "无法获取分支 ${BRANCH} 的最新代码，请确认网络连接及分支是否存在"
+    exit 1
+  fi
   git -C "$INSTALL_DIR" reset --hard "origin/${BRANCH}"
   git -C "$INSTALL_DIR" submodule update --init --recursive
 }
